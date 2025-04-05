ef5c8616df95e0de343ab80c2a6b26737a1246b9
Azim Demian <azimdzhafarovv@gmail.com>
2025-04-05 21:26:50 +0400

    types and constants

---
+++ src/constants.rs
@@ -0,0 +1,38 @@
+use crate::types::{AppState, FeeAmount, PoolConfig, RpcConfig, Token};
+use alloy_primitives::Address;
+
+pub fn get_appstate() -> AppState {
+    let usdt = Token {
+        address: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9"
+            .parse()
+            .unwrap(),
+        symbol: "USDT",
+        name: "Tether",
+        decimals: 6,
+    };
+
+    let usdc = Token {
+        address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
+            .parse()
+            .unwrap(),
+        symbol: "USDC",
+        name: "USD Coin",
+        decimals: 6,
+    };
+
+    let pool = PoolConfig {
+        address: "0xbE3aD6a5669Dc0B8b12FeBC03608860C31E2eef6"
+            .parse()
+            .unwrap(),
+        token0: usdt,
+        token1: usdc,
+        fee: FeeAmount::Low,
+    };
+
+    AppState {
+        rpc: RpcConfig {
+            mainnet: ("https://mainnet.infura.io/v3/04490c8201d34cda8a14532c168b4d51"),
+        },
+        poolcfg: pool,
+    }
+}
---
+++ src/types.rs
@@ -0,0 +1,39 @@
+use alloy_primitives::Address;
+
+#[derive(Debug, Clone)]
+pub struct Token {
+    //Token info
+    pub address: Address,
+    pub symbol: &'static str,
+    pub name: &'static str,
+    pub decimals: u8,
+}
+
+#[derive(Debug, Clone)]
+
+pub enum FeeAmount {
+    Low = 100,      // 0.01%
+    Medium0 = 500,  //0.05%
+    Medium1 = 3000, // 0.3%
+    High = 10000,   // 1%
+}
+
+#[derive(Debug, Clone)]
+pub struct PoolConfig {
+    pub address: Address,
+    pub token0: Token,
+    pub token1: Token,
+    pub fee: FeeAmount,
+}
+
+#[derive(Debug, Clone)]
+pub struct RpcConfig {
+    pub mainnet: &'static str,
+}
+
+#[derive(Debug, Clone)]
+
+pub struct AppState {
+    pub rpc: RpcConfig,
+    pub poolcfg: PoolConfig,
+}
