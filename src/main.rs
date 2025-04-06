535b541d567237dd2eef93f54202eb44aa8f0867
Azim Demian <azimdzhafarovv@gmail.com>
2025-04-06 18:42:18 +0400

    +Types for final data +ABI of contracts

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,16 @@ async fn main() {
     dotenvy::dotenv().ok();
 
     //Load rpc url and provider
-    let rpc_url = env::vat("RPC_URL")?;
+    let rpc_url = env::var("RPC_URL")?;
     let provider = Provider::build(&rpc_url)?;
 
     //Load abi of pool
     let abi_str = file::read_to_string("abi/uniswap_pool.json")?;
     let abi: JsonAbi = serde_json::from_str(&abi_str)?;
 
-    //Creating the inner instance of Pool State
+    //Initializing config
 
-    let state: types::AppConfig = get_appstate(rpc_url);
+    let state: types::AppConfig = get_appstate(&rpc_url);
 
     //Creating pool instance
 
--- src/types.rs
+++ src/types.rs
@@ -28,7 +28,7 @@ pub struct PoolConfig {
 
 #[derive(Debug, Clone)]
 pub struct RpcConfig {
-    pub mainnet: &'static str,
+    pub mainnet: &String,
 }
 
 #[derive(Debug, Clone)]
@@ -36,4 +36,61 @@ pub struct RpcConfig {
 pub struct AppConfig {
     pub rpc: RpcConfig,
     pub poolcfg: PoolConfig,
+}
+
+#[derive(Debug, Clone)]
+pub struct PoolData {
+    //Struct for state of Pool that we're intrested in
+    pub address: Address,
+    pub token0: TokenData,
+    pub token1: TokenData,
+    pub fee: u32,
+    pub tick_spacing: i32,
+    pub max_liquidity_per_tick: u128,
+    pub liquidity: u128,
+    pub slot0: Slot0,
+    pub protocol_fees: ProtocolFees,
+    pub ticks: Vec<TickData>,
+}
+
+#[derive(Debug, Clone)]
+pub struct TokenData {
+    //Struct for the current state of Token
+    pub name: String,
+    pub symbol: String,
+    pub decimals: u8,
+    pub address: Address,
+}
+
+#[derive(Debug, Clone)]
+pub struct Slot0 {
+    //Struct for the current Slot0 info of the pool
+    pub sqrt_price_x96: U256,
+    pub tick: i32,
+    pub observation_index: u16,
+    pub observation_cardinality: u16,
+    pub observation_cardinality_next: u16,
+    pub fee_protocol: u8,
+    pub unlocked: bool,
+}
+
+#[derive(Debug, Clone)]
+pub struct ProtocolFees {
+    //fees of tokens
+    pub token0: U256,
+    pub token1: U256,
+}
+
+#[derive(Debug, Clone)]
+pub struct TickData {
+    //data of ticks of observed Pool
+    pub tick_index: i32,
+    pub liquidity_gross: u128,
+    pub liquidity_net: i128,
+    pub fee_growth_outside_0_x128: U256,
+    pub fee_growth_outside_1_x128: U256,
+    pub tick_cumulative_outside: i64,
+    pub seconds_per_liquidity_outside_x128: u128,
+    pub seconds_outside: u32,
+    pub initialized: bool,
 }
